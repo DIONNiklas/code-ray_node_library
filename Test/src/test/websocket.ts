2a7b35c8275b5328cef3b5c7582693325321122e
Niklas Mengele <niklas.mengele@di-on.solutions>
2021-02-05 11:52:07 +0100

    modified demo project

--- Test/src/test/websocket.ts
+++ Test/src/test/websocket.ts
@@ -3,8 +3,21 @@ const app = express()
 const port = 3000
 const Internet = "Hallo Welt";
 
+
+const doSomething = (): any => {
+    return {
+        username: Math.random()
+    }
+}
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get("/impressum", (req, res) => {
+    const result = doSomething();
+
+    res.send('<h1>Hier steht ein Impressum</h1>' + result);
 })
 
 app.listen(port, () => {
