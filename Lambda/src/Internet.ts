2ebb5e9ab92783cc81aa4724a318ffddf7cbdecd
Niklas Mengele <niklas.mengele@di-on.solutions>
2021-02-15 21:18:51 +0100

    modified demo project

---
+++ Lambda/src/Internet.ts
@@ -0,0 +1,4 @@
+export const Test = (event: any) => {
+    console.log("Internet");
+    return event;
+}
--- Lambda/src/boot/index.ts
+++ Lambda/src/boot/index.ts
@@ -1,5 +1,6 @@
 import {CodeRayLambdaHandler} from "../../../Library";
 import {v1 as uuidv1} from "uuid";
+import {Test} from "../Internet";
 process.env.APPSYNC_DOMAIN_ID = "ajn4wp3fpnaffgifb257lsyrum";
 process.env.AWS_REGION = "eu-west-1";
 process.env.APPSYNC_API_KEY = "da2-6gzaj4bnmnhgveizlali7fprha";
@@ -7,5 +8,5 @@ process.env.APPSYNC_API_KEY = "da2-6gzaj4bnmnhgveizlali7fprha";
 export const handler = CodeRayLambdaHandler((event, context) => {
     const uuid = uuidv1();
 
-    return uuid;
-});
+    return Test(event);
+}, ["2V9FvE0oSoGla8zVgNFACZcFVUsyaW6dYZYY0qJp1zirsCMLnXbuY0IBr4ZL5veUqJSuxJWDMKCjVPF85pdfe74d6J4y7QztunNsH24vc="]);
